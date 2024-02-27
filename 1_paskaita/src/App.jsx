6285b6cd2c649513847032c01faac30afc594577
rytiscer <rytis.cerniauskas@gmail.com>
2024-02-27 21:10:45 +0200

    Added html css to app.jsx and app.css

--- 1_paskaita/src/App.jsx
+++ 1_paskaita/src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="topLine">LOGO HERE</div>
+      <div className="navBar">
+        <a href="#">Home</a>
+        <a href="#">Product</a>
+        <a href="#">Company</a>
+        <a href="#">Contact</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="divForImage">IMG HERE</div>
+      <div className="topSection">
+        <div className="item">
+          <h1>About</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sint
+            porro commodi? Illo libero fuga facere saepe atque sit tempore!
+          </p>
+        </div>
+        <div className="item">
+          <h1>Company</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque eos
+            reiciendis ratione, corporis iusto doloremque esse quaerat
+            consequatur. Sit, et.
+          </p>
+        </div>
+        <div className="item">
+          <h1>Services</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+            tempore ipsum officiis assumenda nesciunt pariatur dolore eius
+            placeat voluptatem quod?
+          </p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="bottomSection">
+        <div className="leftSide">
+          <div className="lsitem">
+            <h1>Content</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+              dignissimos perferendis necessitatibus labore accusantium ut
+              consequuntur quam mollitia sequi distinctio?
+            </p>
+          </div>
+          <div className="lsitem">
+            <h1>Sub Header</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Voluptatibus sint consequuntur iure ad eum veritatis amet! Enim
+              fuga incidunt nihil vero laboriosam deleniti qui, quos repudiandae
+              quis saepe, aperiam ducimus!
+            </p>
+          </div>
+        </div>
+        <div className="rightSide">
+          <h1>Navigation</h1>
+          <div className="nav">
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
